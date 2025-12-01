ca8edba65a981baf17252f4111024dcf7deebb1d
Ikbal Adi Putra <ikbaladip@gmail.com>
2025-12-01 19:11:51 +0700

    memperbaiki redirect url

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import PaymentPage from './pages/PaymentPage'
 
@@ -8,6 +8,9 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/bayar" element={<PaymentPage />} />
+
+        {/* Catch-all untuk semua route yang tidak ada */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
